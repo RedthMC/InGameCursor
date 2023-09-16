2ce57838528f104c53132a34ef6751d531fa9869
Redth <redth1206@proton.me>
2023-09-16 21:24:13 +0800

    v1.0.1

--- src/main/java/me/redth/ingamecursor/InGameCursor.java
+++ src/main/java/me/redth/ingamecursor/InGameCursor.java
@@ -12,7 +12,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Cursor;
 import org.lwjgl.input.Mouse;
+
+import java.nio.IntBuffer;
 
 
 @Mod(modid = InGameCursor.MODID, name = InGameCursor.NAME, version = InGameCursor.VERSION)
@@ -23,13 +27,10 @@ public class InGameCursor {
     public static final NanoVGHelper NANOVG = NanoVGHelper.INSTANCE;
     public static final Minecraft mc = Minecraft.getMinecraft();
 
-    @Mod.Instance(MODID)
-    public static InGameCursor INSTANCE;
     public static ModConfig config;
 
     public static boolean lastEnabled;
 
-    public static boolean hideCursor;
     public static Animation frontAnimation;
     public static Animation backAnimation;
     public static boolean lastButtonDown;
@@ -39,7 +40,8 @@ public class InGameCursor {
     public void onInit(FMLInitializationEvent event) {
         config = new ModConfig();
         EventManager.INSTANCE.register(this);
-        Mouse.setGrabbed(config.enabled);
+        lastEnabled = config.enabled;
+        updateCursor();
         frontAnimation = new EaseInOutQuad(0, ModConfig.frontRadius, ModConfig.frontRadius, false);
         backAnimation = new EaseInOutQuad(0, ModConfig.backRadius, ModConfig.backRadius, false);
     }
@@ -49,16 +51,16 @@ public class InGameCursor {
         if (event.stage != Stage.END) return;
         if (lastEnabled != config.enabled) {
             lastEnabled = config.enabled;
-            Mouse.setGrabbed(lastEnabled);
-            updateCrosshair();
+            updateCursor();
         }
-        if (lastEnabled && (!hideCursor || ModConfig.replaceCrosshair)) NANOVG.setupAndDraw(InGameCursor::drawNano);
+        if (!lastEnabled) return;
+        if (shouldRenderCursor())
+            drawCircle(Mouse.getX(), mc.displayHeight - Mouse.getY());
+        else if (shouldRenderCrosshair())
+            drawCircle(mc.displayWidth / 2, mc.displayHeight / 2);
     }
 
-    public static void drawNano(long vg) {
-        int x = hideCursor ? mc.displayWidth / 2 : Mouse.getX();
-        int y = hideCursor ? mc.displayHeight / 2 : mc.displayHeight - Mouse.getY();
-
+    public static void drawCircle(int mouseX, int mouseY) {
         boolean buttonDown = Mouse.isButtonDown(0) || Mouse.isButtonDown(1) || Mouse.isButtonDown(2);
 
         if (lastButtonDown != buttonDown) {
@@ -72,12 +74,31 @@ public class InGameCursor {
         int frontColor = buttonDown ? ModConfig.clickedFrontColor.getRGB() : ModConfig.frontColor.getRGB();
         int backColor = buttonDown ? ModConfig.clickedBackColor.getRGB() : ModConfig.backColor.getRGB();
 
-        NANOVG.drawCircle(vg, x, y, backAnimation.get(), backColor);
-        NANOVG.drawCircle(vg, x, y, frontAnimation.get(), frontColor);
+        NANOVG.setupAndDraw(vg -> {
+            NANOVG.drawCircle(vg, mouseX, mouseY, backAnimation.get(), backColor);
+            NANOVG.drawCircle(vg, mouseX, mouseY, frontAnimation.get(), frontColor);
+        });
+    }
+
+    public static void updateCursor() {
+        updateCrosshair();
+
+        try {
+            Mouse.setNativeCursor(!lastEnabled ? null : new Cursor(1, 1, 0, 0, 1, IntBuffer.allocate(1), null));
+        } catch (LWJGLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void updateCrosshair() {
         GuiIngameForge.renderCrosshairs = !lastEnabled || !ModConfig.replaceCrosshair;
     }
 
+    public static boolean shouldRenderCrosshair() {
+        return Mouse.isGrabbed() && ModConfig.replaceCrosshair;
+    }
+
+    public static boolean shouldRenderCursor() {
+        return !Mouse.isGrabbed() && Mouse.isInsideWindow();
+    }
 }
